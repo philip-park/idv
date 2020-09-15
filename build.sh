e4bfe3fef4e19534fbef51e4b79a7e320abe9bc9
Philip Park <ppark300@gmail.com>
2020-09-15 07:16:29 -0700

    initial commit

---
+++ build.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+#================================================
+# text attributes ####
+#================================================
+red=`tput setaf 1`
+green=`tput setaf 2`
+yellow=`tput setaf 3`
+blue=`tput setaf 4`
+magenta=`tput setaf 5`
+cyan=`tput setaf 6`
+white=`tput setaf 7`
+blink=`tput blink`
+NC=`tput sgr0`
+
+###################################################################
+# version : version of the idv.sh file
+# cdir    : pionts to current working directory where idv.sh file runs
+# repo    : URL to kernel source repository
+# kdir    : Directory where kernel will be pulled and saved from repo
+# branch  : tag or branch of the kernel source
+# patches : idv patch file name with our ".tar.gz" extention
+###################################################################
+version="0.7"
+cdir=$(pwd)
+echo "cdir : $0"
+#================================================
+# repo, branch, patches, kdir, krevision, kversion
+#================================================
+# iotg repo: "https://github.com/intel/linux-intel-lts.git"
+# iotg idv branch/tag: "lts-v5.4.57-yocto-200819T072823Z"
+#repo="https://github.com/intel/linux-intel-lts.git"
+#branch="lts-v5.4.57-yocto-200819T072823Z"
+#patches=""
+
+# ccp repo: "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git"
+# ccp idv branch/tag: "v5.4.54"
+#repo="https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git"
+#branch="v5.4.54"
+#patches="idv3.0_er3_patchset_rbhe"
+#kdir="kernel"
+
+#krevision="3.0"
+#kversion="intelgvt"
+source ./scripts/kernel-config.sh
+echo "build: patches: $patches"
+
+#exit 0
+#================================================
+# Pull Kernel and Compile
+#================================================
+source ./.idv-config
+source ./scripts/build-helper
+
+#source . `dirname $0`/simple_curses.sh
+exit 0
+
+
+install_packages
+add_modules
+cp -a /usr/share/kernel-package $cdir/ubuntu-package
+pull_kernel
+apply_patches
+kernel_config
+compile_kernel
+
+#================================================
+# Setup Kernel command line option in /etc/default/grub
+#================================================
+source scripts/grub-setup.sh
+grub_setup
+
+echo -en '\n'
+echo "${green}To Install Kernel: \"sudo dpkg -i *.deb\"${NC}"
+
---
+++ scripts/kernel-config.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+kernel_repo+=("CCG-repo" "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git" off \
+              "branch is v5.4.54, IDV patches are needed")
+kernel_repo+=("IOTG-repo" "https://github.com/intel/linux-intel-lts.git" off \
+              "brach is lts-v5.4.57-yocto-200819T072823Z")
+
+default_config=./scripts/idv-config-default
+idv_config_file=./.idv-config
+[[ -f "./.idv-config" ]] && default_config="./.idv-config" 
+source $default_config
+
+#==========================================
+# Set "on" once default found in kernel_repo
+#==========================================
+function set_default_url() {
+  #default_repo=$(grep repo $default_config)
+
+  for (( i=0; i<${#kernel_repo[@]}; i=i+4 )); do
+    [[ ${kernel_repo[$((i+1))]} == $repo ]] && \
+          kernel_repo[$((i+2))]="on" 
+  done
+}
+
+function kernel_options() {
+
+  option=$( dialog --item-help --backtitle "Kernel URL selection" \
+    --radiolist "Kernel can be pulled from two different sources, IOTG and CCG repo\n\
+*IOTG repo: IOTG kernel development team maintains kernel with IDV patches\n\
+  where separate patches is not needed. Warning: the patches update to IDV\n\
+  maintained kernel might be delayed due to extensive testing\n\
+*CCG repo: CCG repo doesn't include the IDV patches needed for GVTg\n\
+  Without the patches, GVTg will not work. \n\
+  Will ask for <patch>.tar.gz file upon exit."  20 80 10 \
+"${kernel_repo[@]}" \
+    3>&1 1>&2 2>&3 )
+
+  case $option in
+    IOTG-repo)
+#      `sed -i "s/^.*repo=.*$/repo/" ./.idv-config` 
+#      `sed -i "s/^.*repo=.*$/repo=https://github.com/intel/linux-intel-lts.git/" ./.idv-config` 
+#      sed -i 's/^.*repo=.*$/repo=\"https://github.com/intel/linux-intel-lts.git\"/' ./.idv-config 
+unset repo
+      echo 'export repo="https://github.com/intel/linux-intel-lts.git"' >> ./temp
+      echo 'export branch="lts-v5.4.57-yocto-200819T072823Z"' >> ./temp
+      echo 'export patches=""' >> ./temp
+
+      ;;
+    CCG-repo)
+      echo 'export repo="https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git"' >> ./temp
+      echo 'export branch="v5.4.54"' >> ./temp
+#    patches="idv3.0_er3_patchset_rbhe"
+
+      # Handle patch file
+      idx=0
+      while IFS=$'\n' read -r line; do
+        [[ $line == $patches ]] && list+=($idx "$line" on) || list+=($idx "$line" off)
+        idx=$((idx+1))
+      done < <(ls *.tar.gz | grep patch)
+
+      option_patch=$(dialog --backtitle "Select patches file" \
+            --radiolist "<patches file name>.tar.gz \n\
+Will ask for <patch>.tar.gz file upon exit."  20 80 10 \
+            "${list[@]}" \
+            3>&1 1>&2 2>&3 )
+        [[ -z $option_patch ]] && patches="" || patches=${list[$((option_patch*3+1))]}
+      ;;
+  esac
+
+  echo "export patches=$patches" >> ./temp
+  echo "$patches"
+#  return 3 
+}
+
+rm ./temp; touch ./temp
+set_default_url
+patches=$(kernel_options)
+source ./temp
+echo "results: patches: '$result', $patches"
+#exit 0
+#kernel_config=(repo branch patches kdir krevision kversion)
+#source $default_config
+ 
+
