b83353d328864338f328e17f628ca3febc6a0d37
ppark300@gmail.com <ppark300@gmail.com>
2020-09-23 00:31:42 -0700

    Change config_kernel.sh to config-kernel.sh

--- config.sh
+++ config.sh
@@ -5,27 +5,43 @@ source scripts/util.sh
 # 
 ###################################################################
 
-#default_config=./scripts/idv-config-default
-#idv_config_file=./.idv-config
-#[[ -f "./.idv-config" ]] && default_config="./.idv-config" || touch ./.idv-config
 
-
-#================================================
-# Clean the mess it made
-#================================================
-#vmroot=/var
-#vmdir=vm
 
 [[ "$1" == "clean" ]] && clean && exit 0
 install_packages
 build_vm_directory
 
-source scripts/config_kernel.sh
+source scripts/config-kernel.sh
 #================================================
 # VGPU mask setting based on mdev_type user input
 #================================================
 source scripts/config-select-vgpu.sh
-#source scripts/setup-vm.sh
 source scripts/config-qemu-setup.sh
 
+function config_main() {
 
+
+cmd=(dialog --keep-tite --menu "Select options:" 22 76 16)
+options=(1 "Kernel Configure"
+         2 "Guest OS 1 configure"
+         3 "Guest OS 2 configure"
+         4 "Guest OS 3 configure")
+
+choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
+
+for choice in $choices
+do
+    case $choice in
+        1) source ./scripts/config-kernel.sh ;;
+        2) 
+            echo "Second Option"
+            ;;
+        3)
+            echo "Third Option"
+            ;;
+        4)
+            echo "Fourth Option"
+            ;;
+    esac
+done
+}
