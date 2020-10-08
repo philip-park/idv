12140c754852c82536a5a610804157d3b578cfc6
ppark300@gmail.com <ppark300@gmail.com>
2020-10-07 23:00:33 -0700

    Initial config-modules.sh

---
+++ scripts/config-modules.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+
+#================================================
+# 3) Add modules for KVM/GVTg
+#================================================
+function add_modules() {
+modules=(kvmgt vfio-iommu-type1 vfio-mdev vfio-pci)
+
+  echo "${green}Adding kernel modules.${NC}"
+  for i in "${modules[@]}"; do
+    echo $i
+    run_as_root "grep -qxF $i /etc/initramfs-tools/modules || echo $i >> /etc/initramfs-tools/modules"
+  done
+}
+
+add_modules
+
