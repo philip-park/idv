7a505092f1472656d64b0f5c0a4f2a147a2eeb14
ppark300@gmail.com <ppark300@gmail.com>
2020-10-20 16:19:02 -0700

    Add clean routine

--- setup.sh
+++ setup.sh
@@ -2,6 +2,17 @@
 
 source ./scripts/util.sh
 
+function clean() {
+  run_as_root "systemctl stop vgpu.service"
+  run_as_root "systemctl disable vgpu.service"
+
+  qemu_service=$( ls  /etc/systemd/system/multi-user.target.wants/qemu@* )
+  for i in $qemu_service; do
+    run_as_root "systemctl stop ${i##*/}"
+    run_as_root "systemctl disable ${i##*/}"
+  done
+}
+
 [[ "$1" == "clean" ]] && clean && exit 0
 
 
