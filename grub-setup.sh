fce3e898996ade8af66ef9ad2f4f914dd0337a37
ppark300@gmail.com <ppark300@gmail.com>
2020-10-20 11:17:05 -0700

    Clean up the grub-setup.sh

--- grub-setup.sh
+++ grub-setup.sh
@@ -34,48 +34,46 @@ function grub_setup() {
 
 
 function update_grub_kernel() {
-unset list
-old_IFS=$IFS
-IFS=$'\n'
-default_kernel=($(grep "GRUB_DEFAULT=" /etc/default/grub))
- # grep strings between '_' and '='
-#default_kernel=( $( grep "FW_VGPU" $idv_config_file | grep -oP '(?<=>).*(?=")' ) )
-echo "default 1: $default_kernel"
-temp="${default_kernel%\"*}"
-default_kernel="${temp##*>}"
-echo "default: $default_kernel"
-kernels=$(grep -E 'menuentry ' /boot/grub/grub.cfg | cut -f 2 -d "'")
-j=0
-#for (( i=0; i<${#kernels[@]}; i++ )); do
-#  [[ " ${kernels[@]} " =~ "Ubuntu, " ]] && list+=( $i "${kernels[$i]}" off "test string" ) 
-#done
+  unset list
 
-for i in ${kernels[@]}; do
-  if [[ $i =~ "Ubuntu, " ]]; then
-    [[ $i == $default_kernel ]] && list+=( $j "$i" on ) \
-      || list+=( $j "$i" off )
+  old_IFS=$IFS
+  IFS=$'\n'
 
-    echo "found" 
-    j=$((j+1))
-  fi
-done
-#IFS=${old_IFS}
+  # detect default kernel by looking at the GRUB_DEFAULT
+  default_kernel=($(grep "GRUB_DEFAULT=" /etc/default/grub))
+  temp="${default_kernel%\"*}"
+  default_kernel="${temp##*>}"
+
+  kernels=$(grep -E 'menuentry ' /boot/grub/grub.cfg | cut -f 2 -d "'")
+  j=0
+
+  for i in ${kernels[@]}; do
+    if [[ $i =~ "Ubuntu, " ]]; then
+      [[ $i == $default_kernel ]] && list+=( $j "$i" on ) \
+        || list+=( $j "$i" off )
+
+      echo "found" 
+      j=$((j+1))
+    fi
+  done
 
   option=$(dialog --backtitle "Select patches file" \
             --radiolist "Select kernel to boot. /etc/default/grub will be updated"  20 80 10 \
             "${list[@]}" \
             3>&1 1>&2 2>&3 )
-echo "options: $option,  ${list[$((option*3+1))]}"
-submenu=($(grep -E 'submenu ' /boot/grub/grub.cfg | cut -f 2 -d "'"))
-grub_default="GRUB_DEFAULT=\"$submenu>${list[$((option*3+1))]}\""
-echo "grub_default: $grub_default"
-IFS=${old_IFS}
-  cp -f $grubfile $tempfile
-    sed -i "/^GRUB_DEFAULT=.*/c $grub_default" $tempfile
-    run_as_root "cp -f $tempfile $grubfile"
-    run_as_root "update-grub2"
+  [[ "$?" -ne 0 ]] && exit 0  # cancel pressed
 
-#    run_as_root "sed -i \"/^GRUB_DEFAULT=.*/c $grub_default\" ./grub"
+  # for now, we assume we have one submenu
+  submenu=($(grep -E 'submenu ' /boot/grub/grub.cfg | cut -f 2 -d "'"))
+  grub_default="GRUB_DEFAULT=\"$submenu>${list[$((option*3+1))]}\""
+
+  echo "grub_default: $grub_default"
+  IFS=${old_IFS}
+
+  cp -f $grubfile $tempfile
+  sed -i "/^GRUB_DEFAULT=.*/c $grub_default" $tempfile
+  run_as_root "cp -f $tempfile $grubfile"
+  run_as_root "update-grub2"
 }
 
 update_grub_kernel
