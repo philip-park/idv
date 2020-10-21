54270d2943a837dfae77bf5d8c0bdab50a5f72a5
Philip Park <ppark300@gmail.com>
2020-10-21 14:18:48 -0700

    Add parameter checking

--- install-quest-os.sh
+++ install-quest-os.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-/var/vm/scripts/install-guest-$i.sh
+source ./scripts/util.sh
+
+[[ $# -eq 0 ]] && echo "Usage: $0 <vgpu1 | vgpu2 | vgpu3>" && exit
+
+case $1 in
+  vgpu1|vgpu2|vgpu3)
+    run_as_root "/var/vm/scripts/install-guest-$1.sh";;
+  *)
+    echo "The parameter should be either vgpu1, vgpu2, or vgpu3";;
+esac
 
