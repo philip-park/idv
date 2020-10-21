19a9729634432440ee5a7b6220eae10b079ea86a
ppark300@gmail.com <ppark300@gmail.com>
2020-10-21 10:04:56 -0700

    Add run_as_root

--- clean.sh
+++ clean.sh
@@ -5,20 +5,21 @@ source $idv_config_file
 
 
 function clean() {
-  sudo apt purge -y docker-ce docker-ce-cli containerd.io
-  sudo apt purge -y dialog
-  sudo apt purge -y acl
-  sudo apt purge -y uuid
-  sudo apt purge -y qemu-system-x86
+  run_as_root "apt purge -y docker-ce docker-ce-cli containerd.io"
+  run_as_root "apt purge -y dialog"
+  run_as_root "apt purge -y acl"
+  run_as_root "apt purge -y uuid"
+  run_as_root "apt purge -y qemu-system-x86"
 
   docker_image_id=$(docker images -q mydocker/bob_the_builder)
-  sudo docker rmi $docker_image_id
+  run_as_root "sudo docker rmi $docker_image_id"
 
   run_as_root "find /var -type d -name \"vm\" -exec sudo rm -rf {} +"
   run_as_root "find . -type d -name $kdir -exec rm -rf {} +"
   find . -type d -name "${patches%.tar.gz}" -exec rm -rf {} +
 #  find . -type f -name "*.deb" -exec rm -rf {} +
   echo   "deb: $cdir/*.deb"
+  run_as_root "apt autoremove"
 }
 
 clean
