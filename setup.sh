0efdb6edeb4cf94fe93075e9d8069156be127758
Philip Park <ppark300@gmail.com>
2020-10-21 13:52:26 -0700

    Add setup to create vm related files

--- setup.sh
+++ setup.sh
@@ -15,7 +15,26 @@ function clean() {
 
 [[ "$1" == "clean" ]] && clean && exit 0
 
+source $cdir/scripts/setup-vm.sh
+#==========================================================
+# Create VM related run time files from user selected FW_VGPU
+# Without FW, the guest OS will not boot. So use FW selection
+# until find better way.
+#==========================================================
+function setup_main() {
+  # grep strings between '_' and '='
+  vgpuinfo=( $( grep "FW_VGPU" $idv_config_file | grep -oP '(?<=_).*(?==)' ) )
+
+	build_create_vgpu
+
+	for vgpu in ${vgpuinfo[@]}; do
+	  create_files "$vgpu"
+  done
+}
+
+
 #if mdev directory not exist then exit
 [[ ! -d /sys/bus/pci/devices/0000:00:02.0/mdev_supported_types ]] && exit 1
 
+setup_main
 source $cdir/systemd/config-systemd.sh
