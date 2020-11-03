c5e58a46c45a3838798a0f1937be329f62a9a152
Philip Park <ppark300@gmail.com>
2020-11-03 11:03:33 -0700

    Kill the process before starting new qemu if already running

--- install-quest-os.sh
+++ install-quest-os.sh
@@ -6,6 +6,9 @@ source ./scripts/util.sh
 
 case $1 in
   vgpu1|vgpu2|vgpu3)
+    # kill if qemu-system-x86_64 for $1 is already running
+    pID=$(ps -ef | grep 'qemu-system-x86_64' | grep $1 | grep -v 'grep' | awk '{ printf $2 }' )
+    run_as_root "kill $pID"
     run_as_root "/var/vm/scripts/install-guest-$1.sh";;
   *)
     echo "The parameter should be either vgpu1, vgpu2, or vgpu3";;
