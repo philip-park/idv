a6b4fc17c279f7d9c9037f95a95d569aa2d69879
Philip Park <ppark300@gmail.com>
2020-11-09 12:58:07 -0700

    Creat a functions

--- build-kernel.sh
+++ build-kernel.sh
@@ -20,36 +20,41 @@ function install_kernel() {
 # check for install option.
 [[ $1 == "install" ]] && install_kernel && exit 0
 
-echo "idv config file: $idv_config_file"
-source $idv_config_file
+function build_kernel() {
+  echo "idv config file: $idv_config_file"
+  source $idv_config_file
 
-#source $cdir/scripts/config-grub.sh
-#source $cdir/scripts/config-modules.sh
+  # if repo is not set, then run config-kernel to get option for kernel repo
+  [[ -z $repo || -z $branch ]] && run_as_root "apt install dialog acl"; source $cdir/scripts/config-kernel.sh
 
-# if repo is not set, then run config-kernel to get option for kernel repo
-[[ -z $repo || -z $branch ]] && run_as_root "apt install dialog acl"; source $cdir/scripts/config-kernel.sh
+  # Install docker to host if not installed
+  source $cdir/scripts/install-docker.sh
 
-# Install docker to host if not installed
-source $cdir/scripts/install-docker.sh
-
-# delete linux*.deb file before building new
-run_as_root "rm -rf linux*.deb"
-run_as_root "rm -rf $kdir"
+  # delete linux*.deb file before building new
+  run_as_root "rm -rf linux*.deb"
+  run_as_root "rm -rf $kdir"
  
-# run docker as user to build kernel
-run_as_root "docker run --rm -v $cdir:/build \
+  # run docker as user to build kernel
+  run_as_root "docker run --rm -v $cdir:/build \
         -u $(id -u ${USER}):$(id -g ${USER}) \
        --name bob mydocker/bob_the_builder"
 #       --name bob mydocker/bob_the_builder  bash -c \"cd /build/docker; ./build-docker.sh\""
+}
 
-debs=$( ls $cdir/*.deb )
-echo "${yellow}$debs${NC}"
+function install_kernel() {
+  debs=$( ls $cdir/*.deb )
+  echo "${yellow}$debs${NC}"
 
-[[ -z "$debs" ]] && echo -e "${red}✖${NC} Oops.. kernel build error" && exit 1
+  [[ -z "$debs" ]] && echo -e "${red}✖${NC} Oops.. kernel build error" && exit 1
 
-read -r -p "\n ${green}✔${NC} Want to install the kernel and reboot? [y/N] " answer
-case "$answer" in
-  [yY]) install_kernel;;
-  *) echo "you can install kernel using ${yellow}$0 install${NC}";;
-esac
+  read -r -p "\n ${green}✔${NC} Want to install the kernel and reboot? [y/N] " answer
+  case "$answer" in
+    [yY]) install_kernel;;
+    *) echo "you can install kernel using ${yellow}$0 install${NC}";;
+  esac
+}
+
+build_kernel
+install_kernel
+
 
